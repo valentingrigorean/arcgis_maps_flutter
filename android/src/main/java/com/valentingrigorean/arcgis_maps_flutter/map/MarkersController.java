f90004fe8ad96429993ef84ffde2192b6fa7c700
Valentin Grigorean <valentin.grigorean1@gmail.com>
2021-08-15 10:31:35 +0300

    - added worker thread for ios/android to parse markers

--- android/src/main/java/com/valentingrigorean/arcgis_maps_flutter/map/MarkersController.java
+++ android/src/main/java/com/valentingrigorean/arcgis_maps_flutter/map/MarkersController.java
@@ -9,10 +9,13 @@ import com.valentingrigorean.arcgis_maps_flutter.Convert;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 import io.flutter.plugin.common.MethodChannel;
 
 public class MarkersController implements MapTouchGraphicDelegate {
+    private static Executor executor = Executors.newSingleThreadExecutor();
 
     private final Map<String, MarkerController> markerIdToController = new HashMap<>();
 
@@ -64,50 +67,57 @@ public class MarkersController implements MapTouchGraphicDelegate {
         if (markersToAdd == null) {
             return;
         }
-        for (Object marker : markersToAdd) {
-            final Map<?, ?> data = (Map<?, ?>) marker;
-            if (data == null) {
-                continue;
+        executor.execute(() -> {
+            for (Object marker : markersToAdd) {
+                final Map<?, ?> data = (Map<?, ?>) marker;
+                if (data == null) {
+                    continue;
+                }
+                final String markerId = (String) data.get("markerId");
+                final MarkerController markerController = new MarkerController(context, markerId, selectionPropertiesHandler);
+                markerIdToController.put(markerId, markerController);
+                Convert.interpretMarkerController(data, markerController);
+                markerController.add(graphicsOverlay);
             }
-            final String markerId = (String) data.get("markerId");
-            final MarkerController markerController = new MarkerController(context, markerId, selectionPropertiesHandler);
-            markerIdToController.put(markerId, markerController);
-            Convert.interpretMarkerController(data, markerController);
-            markerController.add(graphicsOverlay);
-        }
+        });
     }
 
     public void changeMarkers(List<Object> markersToChange) {
         if (markersToChange == null) {
             return;
         }
-        for (Object marker : markersToChange) {
-            final Map<?, ?> data = (Map<?, ?>) marker;
-            if (data == null) {
-                continue;
+        executor.execute(()->{
+            for (Object marker : markersToChange) {
+                final Map<?, ?> data = (Map<?, ?>) marker;
+                if (data == null) {
+                    continue;
+                }
+                final String markerId = (String) data.get("markerId");
+                final MarkerController markerController = markerIdToController.get(markerId);
+                if (markerController != null) {
+                    Convert.interpretMarkerController(data, markerController);
+                }
             }
-            final String markerId = (String) data.get("markerId");
-            final MarkerController markerController = markerIdToController.get(markerId);
-            if (markerController != null) {
-                Convert.interpretMarkerController(data, markerController);
-            }
-        }
+        });
+
     }
 
     public void removeMarkers(List<Object> markerIdsToRemove) {
         if (markerIdsToRemove == null) {
             return;
         }
-        for (Object rawMarkerId : markerIdsToRemove) {
-            if (rawMarkerId == null) {
-                continue;
+        executor.execute(()->{
+            for (Object rawMarkerId : markerIdsToRemove) {
+                if (rawMarkerId == null) {
+                    continue;
+                }
+                final String markerId = (String) rawMarkerId;
+                final MarkerController markerController = markerIdToController.remove(markerId);
+                if (markerController != null) {
+                    markerController.remove(graphicsOverlay);
+                }
             }
-            final String markerId = (String) rawMarkerId;
-            final MarkerController markerController = markerIdToController.remove(markerId);
-            if (markerController != null) {
-                markerController.remove(graphicsOverlay);
-            }
-        }
+        });
     }
 
     public void clearSelectedMarker() {
