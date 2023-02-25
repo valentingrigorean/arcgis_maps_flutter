a25d4001f00762a53c2e4e3d2907c6382ed2476e
Valentin Grigorean <valentin.grigorean1@gmail.com>
2023-02-25 13:53:25 +0200

    Fix crash android

--- android/src/main/java/com/valentingrigorean/arcgis_maps_flutter/map/ArcgisMapController.java
+++ android/src/main/java/com/valentingrigorean/arcgis_maps_flutter/map/ArcgisMapController.java
@@ -59,7 +59,6 @@ final class ArcgisMapController implements DefaultLifecycleObserver, PlatformVie
     private final Context context;
     private final LifecycleProvider lifecycleProvider;
     private final MethodChannel methodChannel;
-
     private final SelectionPropertiesHandler selectionPropertiesHandler;
 
     private final LayersController layersController;
@@ -574,12 +573,12 @@ final class ArcgisMapController implements DefaultLifecycleObserver, PlatformVie
     }
 
     private void destroyMapViewIfNecessary() {
+        mapLoadedListener.setMap(null);
         if (mapView != null) {
             mapView.dispose();
             mapView.removeAllViews();
             mapView = null;
         }
-        mapLoadedListener.setMap(null);
     }
 
 
