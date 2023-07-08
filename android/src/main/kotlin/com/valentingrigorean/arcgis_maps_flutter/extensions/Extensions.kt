20ed2fe82d4c7a6566f9d80ce23aff7d9f6d2bd1
Valentin Grigorean <valentin.grigorean1@gmail.com>
2023-07-08 08:24:13 +0300

    Fix dp/sp impl android

--- android/src/main/kotlin/com/valentingrigorean/arcgis_maps_flutter/extensions/Extensions.kt
+++ android/src/main/kotlin/com/valentingrigorean/arcgis_maps_flutter/extensions/Extensions.kt
@@ -3,13 +3,13 @@ package com.valentingrigorean.arcgis_maps_flutter.extensions
 import android.content.res.Resources
 
 val Int.dp: Int
-    get() = (this / Resources.getSystem().displayMetrics.density).toInt()
+    get() = (this * Resources.getSystem().displayMetrics.density).toInt()
 
 val Float.dp: Float
-    get() = this / Resources.getSystem().displayMetrics.density
+    get() = this * Resources.getSystem().displayMetrics.density
 
 val Int.sp: Float
-    get() = this / Resources.getSystem().displayMetrics.scaledDensity
+    get() = this * Resources.getSystem().displayMetrics.scaledDensity
 
 val Float.sp: Float
-    get() = this / Resources.getSystem().displayMetrics.scaledDensity
+    get() = this * Resources.getSystem().displayMetrics.scaledDensity
