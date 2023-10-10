efec550cdc08cd31817dbcd88606a3843e075eef
Valentin Grigorean <valentin.grigorean1@gmail.com>
2023-10-10 09:41:32 +0300

    Fix android date time parsing

--- android/src/main/kotlin/com/valentingrigorean/arcgis_maps_flutter/convert/DateConvert.kt
+++ android/src/main/kotlin/com/valentingrigorean/arcgis_maps_flutter/convert/DateConvert.kt
@@ -1,13 +1,15 @@
 package com.valentingrigorean.arcgis_maps_flutter.convert
 
 import java.time.Instant
+import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 fun Instant.toFlutterValue() : String{
     val formatter = DateTimeFormatter.ISO_INSTANT
     return formatter.format(this)
 }
 
-fun String.fromFlutterInstant() : Instant{
-    val formatter = DateTimeFormatter.ISO_INSTANT
-    return formatter.parse(this, Instant::from)
+fun String.fromFlutterInstant() : Instant {
+    val truncatedString = this.substring(0, this.length.coerceAtMost(23))
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS").withZone(ZoneId.of("UTC"))
+    return Instant.from(formatter.parse(truncatedString))
 }
