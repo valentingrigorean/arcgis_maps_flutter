8dccdc9f37bdbd7a11b5960751eb4f28c3622af2
Valentin Grigorean <valentin.grigorean1@gmail.com>
2024-04-26 17:47:24 +0300

    Increment version

--- android/src/main/kotlin/com/valentingrigorean/arcgis_maps_flutter/convert/ThrowableConvert.kt
+++ android/src/main/kotlin/com/valentingrigorean/arcgis_maps_flutter/convert/ThrowableConvert.kt
@@ -2,7 +2,7 @@ package com.valentingrigorean.arcgis_maps_flutter.convert
 
 fun Throwable.toFlutterJson(withStackTrace: Boolean = true,addFlutterFlag: Boolean = true): Any {
     val json = HashMap<String, Any>(2)
-    json["errorMessage"] = localizedMessage
+    json["errorMessage"] = localizedMessage ?: "Unknown error"
     if (withStackTrace) {
         json["nativeStackTrace"] = stackTraceToString()
     }
